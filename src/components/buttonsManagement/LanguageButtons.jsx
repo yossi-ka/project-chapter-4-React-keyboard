4f4f6566602121c3cc9ed8ce94d11ff6c7c10aee
yossi-ka <yk111067@gmail.com>
2024-08-06 14:21:25 +0300

    הוספת תסריט קומפוננטות

--- src/components/buttonsManagement/LanguageButtons.jsx
+++ src/components/buttonsManagement/LanguageButtons.jsx
@@ -1,18 +1,41 @@
 import classes from "../Keyboard.module.css";
 function LanguageButtons(props) {
+  const changeLanguage = (num) => {
+    props.setKeyboardType(num);
+  };
 
-  const changeLanguage=(num)=>{
-    props.setKeyboardType(num)
-  }
-  
   return (
     <div className={classes.languages}>
-      <button onClick={()=>{changeLanguage(0)}}>עברית</button>
-      <button onClick={()=>{changeLanguage(1)}}>English - up</button>
-      <button onClick={()=>{changeLanguage(2)}}>English - low</button>
-      <button onClick={()=>{changeLanguage(3)}}>Symbols</button>
+      <button
+        onClick={() => {
+          changeLanguage(0);
+        }}
+      >
+        עברית
+      </button>
+      <button
+        onClick={() => {
+          changeLanguage(1);
+        }}
+      >
+        English - up
+      </button>
+      <button
+        onClick={() => {
+          changeLanguage(2);
+        }}
+      >
+        English - low
+      </button>
+      <button
+        onClick={() => {
+          changeLanguage(3);
+        }}
+      >
+        Symbols
+      </button>
     </div>
   );
 }
 
-export default LanguageButtons
+export default LanguageButtons;
