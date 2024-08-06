e85b41c9098c8775886ba7c56efd61322cba7393
yitzchak <yitz3992@gmail.com>
2024-08-06 13:51:10 +0300

    add language buttons

--- src/components/buttonsManagement/LanguageButtons.jsx
+++ src/components/buttonsManagement/LanguageButtons.jsx
@@ -1,11 +1,16 @@
-import classes from "./Keyboard.module.css";
-function LanguageButtons() {
+import classes from "../Keyboard.module.css";
+function LanguageButtons(props) {
+
+  const changeLanguage = (num)=>{
+    props.setKeyboardType(num)
+  }
+
   return (
     <div className={classes.languages}>
-      <button>Hebrew</button>
-      <button>English - up</button>
-      <button>English - low</button>
-      <button>Symbols</button>
+      <button onClick={()=>{changeLanguage(0)}}>Hebrew</button>
+      <button onClick={()=>{changeLanguage(1)}}>English - up</button>
+      <button onClick={()=>{changeLanguage(2)}}>English - low</button>
+      <button onClick={()=>{changeLanguage(3)}}>Symbols</button>
     </div>
   );
 }
